0a191a0965b2cb295b1e56ab3ccb0d718ee53141
mochi-dot-py <65631838+mochi-dot-py@users.noreply.github.com>
2020-12-16 20:52:00 -0500

    Created a webmanifest

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,8 +4,8 @@ const FILES_TO_CACHE = [
   'index.html',
   'index.js',
   'styles.css',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png',
+  '/assets/icons/icon-192x192.png',
+  '/assets/icons/icon-512x512.png',
   'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
   'https://cdn.jsdelivr.net/npm/chart.js@2.8.0',
 ]
